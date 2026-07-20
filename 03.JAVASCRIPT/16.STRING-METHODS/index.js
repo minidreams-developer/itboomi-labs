5b7dd32701ec3a1f5fbc060ca0d83040acf60e47
arunprince-minidreams <arunprince@itboomitech.in>
2026-07-20 20:10:38 +0530

    String methods basic completed

---
+++ 03.JAVASCRIPT/16.STRING-METHODS/index.js
@@ -0,0 +1,49 @@
+  /*
+            Strings
+            1. Double quotes
+            2. Single quotes
+            3. Backticks
+
+
+            Strings are immutable => you can't change the substring of a string.
+
+            string => "hellojohnworld"
+            substring => "s         t"
+
+            let fruit = apple; 
+               fruit = mango;
+
+               "Hello" => H-e-l-l-o
+                   pos => 0-1-2-3-4
+
+
+            Accessing strings
+            variable[pos]
+
+        
+        
+        */
+
+
+        let fruits = "Mango";
+        console.log("string=>", fruits)
+
+        // console.log(fruits[2]);
+
+
+        for(let i = 0; i < fruits.length; i++){
+            console.log(fruits[i])
+        }
+        
+
+
+        // String methods
+        // string.toUppoerCase();
+
+
+        let name = "adhUTJLGgj";
+        console.log(name.toUpperCase());
+        console.log(name.toLowerCase());
+
+        console.log("welcome nodemon")
+        
