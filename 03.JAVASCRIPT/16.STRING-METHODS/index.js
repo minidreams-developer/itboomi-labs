d62fcab2a7f6eb099b0b1021264d86365c084865
arunprince-minidreams <arunprince@itboomitech.in>
2026-07-21 19:55:23 +0530

    String methods in progress

--- 03.JAVASCRIPT/16.STRING-METHODS/index.js
+++ 03.JAVASCRIPT/16.STRING-METHODS/index.js
@@ -1,4 +1,4 @@
-  /*
+/*
             Strings
             1. Double quotes
             2. Single quotes
@@ -24,26 +24,75 @@
         
         */
 
+let fruits = "Mango";
+console.log("string=>", fruits);
 
-        let fruits = "Mango";
-        console.log("string=>", fruits)
+// console.log(fruits[2]);
 
-        // console.log(fruits[2]);
+for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}
+
+// String methods
+// string.toUppoerCase();
+
+let name = "adhUTJLGgj";
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
+
+console.log("welcome nodemon");
+
+/*
+            Finding a substring
+            str.indexOf(a, b)=> if it found substring, return index or position. If not , then return -1;
+            a => search string
+            b => search start position
 
 
-        for(let i = 0; i < fruits.length; i++){
-            console.log(fruits[i])
-        }
+            str.includes(a, b) => return true if substring exist or else return false.
+            a => search string
+            b => search start position
         
-
-
-        // String methods
-        // string.toUppoerCase();
-
-
-        let name = "adhUTJLGgj";
-        console.log(name.toUpperCase());
-        console.log(name.toLowerCase());
-
-        console.log("welcome nodemon")
         
+        
+        */
+
+let str = "Hello, World";
+// 0123456789,,,,
+console.log("Finding substring=>", str.indexOf("World"));
+
+console.log("Finding substring=>", str.indexOf("o  d", 8));
+
+console.log("Using Includes=>", str.includes("d", 11));
+
+/*
+            Extracting substring
+            1. str.slice()
+            2. str.substring()
+            3. str.substr()
+
+
+            str.slice(a, b) => return "substring" in the range from a postion to b-1 position.
+            a => index or pos
+            b => index or pos
+        
+            a=1, b=6  =>123456
+
+
+            str.substr(a, b) => return substring
+            a => index
+            b => length
+        
+        */
+
+// "Hello, World"
+//  0123456789
+console.log("Extract using slice()=>", str.slice(0, 4)); // Hell0
+
+console.log("Extract using slice-negative =>", str.slice(-4)); // str.substring(-6) => str.substring(0)
+
+
+console.log("Extract using substring()=>", str.substring(0, 4));
+
+console.log("Extracting using substr=>", str.substr(0,4));
+
