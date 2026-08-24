4ff9bd09cde565e044aec94d90ec70b6fa337593
arunprince-minidreams <arunprince@itboomitech.in>
2026-08-24 15:24:05 +0530

    OS-core modules node.js completed

---
+++ 05.NODE/03.CORE-MODULES/01.OS/os.js
@@ -0,0 +1,12 @@
+// os - core library
+const os = require("os");
+
+// console.log("os===>", os);
+
+// os.type()
+// os.platform()
+
+console.log("os type:", os.type());
+console.log("os platform:", os.platform());
+console.log("Arch:", os.arch());
+console.log("cpu:", os.cpus());
